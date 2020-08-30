a6ce8959e129acde09c178e238b8ce3b3b81f225
Asanka Siriwardena <asankabg@gmail.com>
2020-08-30 21:41:54 +0530

    Spring security LDAP authentication sample project

---
+++ src/main/java/org/asankasi/ldap/HomeController.java
@@ -0,0 +1,12 @@
+package org.asankasi.ldap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "<h1>Welcome Home</h1>";
+	}
+} 
---
+++ src/main/java/org/asankasi/ldap/SecurityConfigurator.java
@@ -0,0 +1,38 @@
+package org.asankasi.ldap;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfigurator extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(getPasswordEncoder())
+                .passwordAttribute("userPassword");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and().formLogin();
+    }
+
+    @Bean
+    protected PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
